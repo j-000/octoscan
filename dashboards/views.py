43944a40411565577fd8f8266482adbee976d57b
j-000 <jjasilva85@gmail.com>
2020-03-20 09:13:27 +0000

    Refactored views to use Django's generics

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework import views
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .serializers import DashboardSerializer, PageSerializer
